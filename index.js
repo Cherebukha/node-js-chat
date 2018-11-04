9bcdc72e51e5aea1d89ca0125f23ac1ad1ad4d6e
Cherebukha <cherebukha@gmail.com>
2018-11-04 16:57:20 +0700

    Minor modifications applied

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var userList = [];
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket) {
+	io.emit('update user list', userList);
 	socket.on('set nickname', function(name) {
 		io.emit('join', 'joined', name);
+		userList[userList.length] = name;
 		console.log('User joined: ' + name);
 		io.emit('set nickname', name);
+		io.emit('update user list', userList);
 		socket.on('send message', function(msg) {
 			console.log(name + ': ' + msg);
 			io.emit('send message', name, msg);
 		});
 		socket.on('disconnect', function() {
+			var index = userList.indexOf(name);
+			if (index >= 0) userList.splice(index, 1);
 			console.log('User left: ' + name);
 			io.emit('join', 'left', name);
+			io.emit('update user list', userList);
 		});
 	});
 });
