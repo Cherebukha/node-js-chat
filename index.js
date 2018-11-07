0effda2b7fa3e972a59e31cfb67f00f3fd15ebb8
Cherebukha <cherebukha@gmail.com>
2018-11-07 16:40:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ io.on('connection', function(socket) {
 });
 
 http.listen(3000, function() {
+	appendToFile('history/chat.txt', getCurrentTime() + ' It\'s working...\n');
 	console.log('Прослушивание порта 3000');
 });
 
@@ -46,7 +47,7 @@ function appendToFile(file, text) {
 
 function getCurrentTime() {
 	var date = new Date(), hour = date.getHours(), minute = date.getMinutes(), second = date.getSeconds(), day = date.getDay(), month = date.getMonth(), year = date.getFullYear();
-	if (hour < 10) hours = '0' + hour;
+	if (hour < 10) hour = '0' + hour;
 	if (minute < 10) minute = '0' + minute;
 	if (second < 10) second = '0' + second;
 	if (day < 10) day = '0' + day;
